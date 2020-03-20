b486a59b368ad2619e4ea608b12e0450bb543920
PaRaDoX <paradoxlpu@gmail.com>
2020-03-20 16:50:03 +0300

    Init Project

---
+++ RegistrationTestApp/RegistrationTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegistrationTestApp
+//
+//  Created by Вячеслав on 20/03/2020.
+//  Copyright © 2020 PaRaDoX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
